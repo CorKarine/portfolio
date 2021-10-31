1ffa15f9890dfed506d4ab72aaca178a6d5f91f2
CorKarine <70386393+CorKarine@users.noreply.github.com>
2021-10-30 21:33:25 -0400

    transfer

---
+++ script.js
@@ -0,0 +1,23 @@
+var swiperWeb = new Swiper(".swiper-web", {
+    slidesPerView: 6,
+    spaceBetween: 39,
+    loop: true,
+    speed:2000,
+    simulateTouch:false,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false,
+      },
+  });
+
+  var swiperProgramme = new Swiper(".swiper-programme", {
+    slidesPerView: 6,
+    spaceBetween: 39,
+    loop: true,
+    speed:2000,
+    simulateTouch:false,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false,
+      },
+  });
