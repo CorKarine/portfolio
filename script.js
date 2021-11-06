e4a9724ae922ab852ecb29561c99a5d8584e9dfe
CorKarine <70386393+CorKarine@users.noreply.github.com>
2021-11-06 17:34:49 -0400

    fix bootstrap et responsive

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const lettres = document.querySelectorAll('lettre');
 var swiperWeb = new Swiper(".swiper-web", {
-    slidesPerView: 6,
+    slidesPerView: 'auto',
     spaceBetween: 39,
     loop: true,
-    speed:2000,
+    speed:4000,
     simulateTouch:false,
     autoplay: {
         delay: 1,
@@ -11,13 +12,45 @@ var swiperWeb = new Swiper(".swiper-web", {
   });
 
   var swiperProgramme = new Swiper(".swiper-programme", {
-    slidesPerView: 6,
+    slidesPerView: 'auto',
     spaceBetween: 39,
     loop: true,
-    speed:2000,
+    speed:4000,
     simulateTouch:false,
     autoplay: {
         delay: 1,
         disableOnInteraction: false,
       },
   });
+
+
+  const animation= gsap.timeline({
+    defaults: { 
+      y: '50px',
+      ease: 'none',
+      repeat:-1,
+      yoyo:true,
+      duration:1
+    }
+  })
+    .from('.prenomK', {})
+    .from('.prenomA', {}, "<0.2")
+    .from('.prenomR', {}, "<0.2")
+    .from('.prenomI', {}, "<0.2")
+    .from('.prenomN', {}, "<0.2")
+    .from('.prenomE', {}, "<0.2")
+    .from('.nomC', {}, "<0.4")
+    .from('.nomO', {}, "<0.2")
+    .from('.nomR1', {}, "<0.2")
+    .from('.nomM', {}, "<0.2")
+    .from('.nomI', {}, "<0.2")
+    .from('.nomE', {}, "<0.2")
+    .from('.nomR2', {}, "<0.2");
+
+animation.play();
+lettres.forEach(lettre => {
+  
+lettre.addEventListener('mouseEnter', function(){
+  animation.pause();
+})
+});
